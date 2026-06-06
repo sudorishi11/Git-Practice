b088c7d16f0891ba8c261d24117af1ebf439276f
sudorishi11 <bharadwajrishi24@gmail.com>
2026-06-06 19:38:45 +0530

    Understanding variables...

---
+++ shell-scripting/variables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Understanding variables in Shell scripitng ----
+
+Name="Rishi Kumar Pathak"
+UserId="sudorishi11"
+Age=20
+Current_year="3rd year"
+
+#calling variables using $ sign----
+
+echo "I'm $Name, I'm $Age years old"
+sleep 1s
+echo "I'm currently in $Current_year "
+sleep 1s
+echo "My git-hub userId is $UserId"
+
